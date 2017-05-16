b2943714295c0fd0eb911304c8b5e77f64eb52b7
Taras Kredens <taras.kredens@gmail.com>
2017-05-16 22:44:04 +0300

    Add BusinessCard Class

--- Purse/Program.cs
+++ Purse/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Money1;
 using CreditCard1;
+using BusinessCard;
 
 namespace Purse
 {
@@ -12,7 +13,7 @@ namespace Purse
     {
         static void Main(string[] args)
         {
-            var money = new Money1.Money(100);
+            var money = new Money1.Money(1000);
             Console.WriteLine(money.GetMoney());
             var credit = new CreditCard1.CreditCard(500);
             Console.WriteLine(credit.GetSumCreditCard());
